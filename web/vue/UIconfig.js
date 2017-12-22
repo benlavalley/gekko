2da830b23bb656a7b83d60791283f22982ce6d28
ben <ben@nibbl.com>
2017-12-21 22:45:21 -0500

    run headless

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -4,9 +4,9 @@
 // see https://github.com/askmike/gekko/blob/stable/docs/installing_gekko_on_a_server.md
 
 const CONFIG = {
-  headless: false,
+  headless: true,
   api: {
-    host: '127.0.0.1',
+    host: '0.0.0.0',
     port: 4000,
     timeout: 120000 // 2 minutes
   },
